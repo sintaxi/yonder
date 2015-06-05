46206b7dde3f071d110ee1a6fef30cd4a1edf467
Brock Whitten <brock@chloi.io>
2015-06-05 14:37:33 -0700

    middleware now configurable

--- test/middleware.js
+++ test/middleware.js
@@ -1,10 +1,10 @@
 var express = require("express")
 var yonder  = require("../").middleware
 var request = require("request")
-var port    = 9966
+
 
 describe("middleware", function(){
-
+  var port    = 9966
   before(function(done){
     express()
       .use(yonder(__dirname + "/examples/blog.redirects"))
@@ -15,7 +15,32 @@ describe("middleware", function(){
 
 
   it("should obey redirect", function(done){
-    request.get("http://localhost:9966/foo", { followRedirect: false }, function(e,r,b){
+    request.get("http://localhost:" + port + "/foo", { followRedirect: false }, function(e,r,b){
+      r.statusCode.should.eql(302)
+      r.headers.should.have.property("location", "/bar")
+      done()
+    })
+  })
+
+})
+
+
+describe("custom middleware", function(){
+
+  var port = 9967
+  before(function(done){
+    express()
+      .use(yonder(function(req, rsp, cb){
+        cb([[302, "/*", "/bar"]])
+      }))
+      .listen(port, function(){
+        done()
+      })
+  })
+
+
+  it("should obey redirect", function(done){
+    request.get("http://localhost:" + port + "/foo", { followRedirect: false }, function(e,r,b){
       r.statusCode.should.eql(302)
       r.headers.should.have.property("location", "/bar")
       done()
